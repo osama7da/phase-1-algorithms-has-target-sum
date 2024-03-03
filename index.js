53b248094b92a2cfbcd40c44312cff4b5a333724
Osama daoud <osamadaoud66@gmail.com>
2024-03-03 18:29:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Write your algorithm here 
+  for(let i=0 ; i<array.length ; i++){
+    const complement = target - array[i]
+    for (let j=i+1 ; j<array.length ; j++){
+      if(array[j]=== complement) return true
+    }
+  } 
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here 
+  
 */
 
 /* 
   Add your pseudocode here
+  I have to write a function that has 2 arguemnt , the first is array and the second is the target number , 
+  we have to check if we can add 2 numbers from the array that give us the target and print true , if not print false 
 */
 
 /*
   Add written explanation of your solution here
+  iterat through the array 
+  check for the complement 
+  check for the rest element 
+  print the result 
+
+
+
 */
 
 // You can run `node index.js` to view these console logs
